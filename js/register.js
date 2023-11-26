15adadf0d8bec395bc0eb8204c872a31b164e9d6
Joaquin Talice <joaquintalice@gmail.com>
2023-11-26 18:51:21 -0300

    feat: signup alert's modal

--- js/register.js
+++ js/register.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', main);
 function main() {
     const signupForm = document.getElementById('signup-form');
     signupForm.addEventListener('submit', handleSignup)
+    termsAndPols()
 }
 
 async function handleSignup(e) {
@@ -10,17 +11,21 @@ async function handleSignup(e) {
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
 
-    if (!emailInput.value && !passwordInput.value) return alert('*simulador de alerta*Poné algo crack');
-    if (!emailInput.value || !passwordInput.value) return alert('*simulador de alerta*Te faltó algún campo jeje')
-    if (!validateEmail(emailInput.value)) return alert('*simulador de alerta*Ingresa un email válido')
+    if ((!emailInput.value && !passwordInput.value) ||
+        (!emailInput.value || !passwordInput.value)) return showModal('Campos inválidos.', 'Ninguno de los dos campos debe estar vacío.')
+    if (!validateEmail(emailInput.value)) return showModal('Campos inválidos.', 'Ingresa un email válido.')
 
     const newUser = await signupUser(emailInput.value, passwordInput.value);
-    if (!newUser) return alert('*simulador de alerta* Hubo algún error en el registro')
-    alert('*simulador de alerta* Te registraste con éxito')
+    if (!newUser) return;
+    showModal('Registro exitoso.', 'Te registraste con éxito. Redirigiendo...')
+    setTimeout(() => {
+        location.href = 'signin.html'
+    }, 1500);
 }
 
 async function signupUser(email, password) {
-    const SIGNUP_ENDPOINT = 'https://sem20-2-dev-zgcj.4.us-1.fl0.io/auth/signup'
+    // const SIGNUP_ENDPOINT = 'https://sem20-2-dev-zgcj.4.us-1.fl0.io/auth/signup'
+    const SIGNUP_ENDPOINT = 'http://localhost:3005/auth/signup'
     const options = {
         method: 'POST',
         headers: {
@@ -30,11 +35,39 @@ async function signupUser(email, password) {
     }
 
     const res = await fetch(SIGNUP_ENDPOINT, options);
-    if (!res.ok) return
+    console.log(res)
+    if (res.status === 409) return showModal('Error.', 'Ya existe un usuario con este correo.')
+    if (!res.ok) return showModal('Error.', 'Hubo algún error en el registro')
     const data = await res.json();
-    location.href = 'signin.html'
     return data
 }
+
+function termsAndPols() {
+    const terms = document.getElementById('terms')
+    const pols = document.getElementById('pols')
+
+    terms.addEventListener('click', () => {
+        showModal('Términos de uso', 'No subir cosas raras.')
+    })
+    pols.addEventListener('click', () => {
+        showModal('Políticas de privacidad', 'lorem ipsum...')
+    })
+
+}
+
+function showModal(title, description) {
+    const modalTitle = document.getElementById('modal-title');
+    const modalDescription = document.getElementById('modal-description');
+    const alertModal = new bootstrap.Modal(document.getElementById('alertModal'));
+
+    modalTitle.textContent = title;
+    modalDescription.textContent = description;
+
+    alertModal.show();
+}
+
+
+
 
 const validateEmail = (email) => {
     return String(email)
